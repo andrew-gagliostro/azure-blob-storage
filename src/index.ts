3275928e789d1805ad2aafdbf210359dc924af22
andrew-gagliostro <argagliostro@gmail.com>
2022-11-22 18:38:26 -0500

    updated blob storage strategy

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { DataLakeServiceClient, FileReadResponse } from '@azure/storage-file-datalake';
 import { ChainedTokenCredential, DefaultAzureCredential, ManagedIdentityCredential } from '@azure/identity';
+import { BlobServiceClient } from '@azure/storage-blob';
 import * as fs from "fs";
 
 export type clientInput = {
@@ -10,81 +10,77 @@ export type clientInput = {
 export class AzureBlobClient {
   blob_cs: string;  
   managed_identity_toggle: boolean;
-  managed_identity?: ManagedIdentityCredential;
-  credential_chain?: ChainedTokenCredential;
-  default_credential?: DefaultAzureCredential;
+  blob_service_client: BlobServiceClient;
 
 
   //class constructor
   constructor(input: clientInput) {
     this.blob_cs = input.blob_cs;
     this.managed_identity_toggle = input.managed_identity_toggle //os.environ.get("managed_identity");
-
+    let default_credential = new DefaultAzureCredential();
     if (this.managed_identity_toggle) {
-      this.managed_identity = new ManagedIdentityCredential();
-      this.credential_chain = new ChainedTokenCredential(this.managed_identity);
-      this.default_credential = new DefaultAzureCredential();
+      let managed_identity = new ManagedIdentityCredential();
+      let credential_chain = new ChainedTokenCredential(managed_identity);
+      this.blob_service_client = new BlobServiceClient(this.blob_cs, managed_identity)
+    }
+    else {
+      this.blob_service_client = BlobServiceClient.fromConnectionString(this.blob_cs);
     }
   }
 
-  public async store_document(path_full: string, file_obj: File | null, fileSystemName: string) {
-    var datalake_service_client, dir_client, file_client, file_system_client, file_name, path_base;
-    path_base = path_full.split("/", 1)[0];
-    file_name = path_full.split("/", 1)[1];
-    datalake_service_client = DataLakeServiceClient.fromConnectionString(this.blob_cs);
+  public async store_document(containerName: string, blobName: string, blob_obj: Buffer): Promise<boolean> {
 
-    file_system_client = datalake_service_client.getFileSystemClient(fileSystemName);
-    dir_client = file_system_client.getDirectoryClient(path_base);
-    file_client = dir_client.getFileClient(file_name)
+    try {
 
+      let containerClient = this.blob_service_client.getContainerClient(containerName);
 
-    if (file_obj !== null) {
-      file_client.append(file_obj, 0, file_obj.size);
-      file_client.flush(file_obj.size);
-    } else {
-      const data = fs.readFileSync(path_full)
-      file_client.append(data, 0, data.length);
-      file_client.flush(data.length);
-    }
-    console.log(`Create and upload file ${path_full} successfully`);
-    return true;
-  }
-
-  public async fetch_document(path_full: string, fileSystemName: string): Promise<Blob> {
-    var path_base;
-    path_base = path_full.split("/", 1)[0];
-    file_name = path_full.split("/", 1)[1];
-    fs.mkdir(path_base, (err) => {
-      if (err) {
-          return console.error(err);
+      let exists: boolean = await containerClient.exists()
+      if(!(exists)){
+        await containerClient.createIfNotExists();
       }
-      console.log('Directory created successfully!');
-    });
-    var datalake_service_client, dir_client, file_client, file_system_client, file_name;
+      console.log(`Created container client with name ${containerClient.containerName}`)
 
-    datalake_service_client = DataLakeServiceClient.fromConnectionString(this.blob_cs);
-    file_system_client = datalake_service_client.getFileSystemClient(fileSystemName);
-    dir_client = file_system_client.getDirectoryClient(path_base);
-    file_client = dir_client.getFileClient(file_name)
+      const blockBlobClient = containerClient.getBlockBlobClient(blobName);
 
-    const downloaded_res: FileReadResponse = await file_client.read();
-    let y: Blob = new Blob();
-    let res_blob: Blob | undefined = await downloaded_res.contentAsBlob
-    let res_str:string = await this.blobToString(res_blob ? res_blob : y);
-    console.log(res_str);
+      const uploadBlobResponse = await blockBlobClient.upload(blob_obj, blob_obj.length)
 
-    return res_blob ? res_blob : y;
+      console.log(`Created and uploaded file ${containerName}/${blobName} successfully`, JSON.stringify(uploadBlobResponse));
+      return true;
+    } catch(e) {
+      console.log(e)
+      throw e;
+      return false;
+    }
+
   }
 
-  public async blobToString(blob: Blob): Promise<string> {
-    const fileReader = new FileReader();
-    return new Promise<string>((resolve, reject) => {
-      fileReader.onloadend = (ev: any) => {
-        resolve(ev.target!.result);
-      };
-      fileReader.onerror = reject;
-      fileReader.readAsText(blob);
-    });
+  public async fetch_document(containerName: string, blobName: string): Promise<Buffer> {
+
+    try{
+      let containerClient = this.blob_service_client.getContainerClient(containerName);
+      const blobClient = containerClient.getBlobClient(blobName);
+      const downloadBlockBlobResponse = await blobClient.download();
+      const res: Buffer = await this.streamToBuffer(downloadBlockBlobResponse.readableStreamBody)
+      console.log("Downloaded blob content:", res.toString());
+      return res; 
+    }
+    catch(e) {
+      console.log(e)
+      throw e;
+    }
   }
 
+
+  public async streamToBuffer(readableStream: any): Promise<Buffer> {
+    return new Promise<Buffer>((resolve, reject) => {
+      const chunks: any[] = [];
+      readableStream.on("data", (data: any) => {
+        chunks.push(data instanceof Buffer ? data : Buffer.from(data));
+      });
+      readableStream.on("end", () => {
+        resolve(Buffer.concat(chunks));
+      });
+      readableStream.on("error", reject);
+    });
+  }
 }
