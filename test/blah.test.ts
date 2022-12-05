c5a7886f84a01ecc1f155cbd112c515f3eeb13af
andrew-gagliostro <77214948+andrew-gagliostro@users.noreply.github.com>
2022-12-04 22:58:12 -0500

    Update blah.test.ts

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,8 +1,8 @@
-import { AzureBlobClient } from '../src';
+import { BlobClient } from '../src';
 
 describe('blah', () => {
   it('works', () => {
-    let client = new AzureBlobClient({blob_cs: "UseDevelopmentStorage=true", managed_identity_toggle: false});
+    let client = new BlobClient({blob_cs: "UseDevelopmentStorage=true", managed_identity_toggle: false});
     console.log("test pass")
   });
 });
